4dd4f332abe65b66a2fe3776aae91e524ef451f8
Fajrendajs <40194093+Fajrendajs@users.noreply.github.com>
2019-09-01 15:31:33 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
-var http = require("http");
+const rp = require("request-promise");
+const $ = require("cheerio");
+//const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
+const url = "https://www.pro-football-reference.com/years/2018/games.htm";
 
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
+rp(url)
+  .then(function(html) {
+    //success!
+    console.log(
+      $(
+        "#games > tbody:nth-child(4) > tr > td:nth-child(8) > a:nth-child(1)",
+        html
+      ).length
+    );
+    const boxscoreUrls = [];
+    for (let i = 0; i < 267; i++) {
+      boxscoreUrls.push(
+        $(
+          "#games > tbody:nth-child(4) > tr > td:nth-child(8) > a:nth-child(1)",
+          html
+        )[i].attribs.href
+      );
+    }
+    console.log(boxscoreUrls);
+
+    //console.log(html);
   })
-  .listen(8080); //the server object listens on port 8080
+  .catch(function(err) {
+    //handle error
+  });
